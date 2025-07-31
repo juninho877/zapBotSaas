918b9f42cb3f7fbb2e57715a6024e39a20d0539d
juninho877 <alessandrosandrinho877@gmail.com>
2025-07-31 20:42:58 -0300

    Corrigir entry point do Vite e chave duplicada

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'build',
     assetsDir: 'static',
     sourcemap: false,
     rollupOptions: {
+      input: './index.html',
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
